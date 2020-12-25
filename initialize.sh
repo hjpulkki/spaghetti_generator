4844d3b139c0bfee0885219b5471e80c55ad2314
Heikki Pulkkinen <h@eikki.net>
2020-12-25 22:42:44 +0200

    First working version for basic recepies

---
+++ initialize.sh
@@ -0,0 +1,3 @@
+wget -O data/vanilla-1.0.0.json https://raw.githubusercontent.com/KirkMcDonald/kirkmcdonald.github.io/master/data/vanilla-1.0.0.json
+
+wget -O src/factorio_blueprints.py https://raw.githubusercontent.com/ericmburgess/python-factorio/master/sources/factorio/blueprints.py
